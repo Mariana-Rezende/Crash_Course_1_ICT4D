a937254075943501870e23823e739a442885c5fb
Mariana Villas Boas Rezende <mariana.rezende@yhub.com.br>
2023-03-05 11:05:52 -0300

    final commit

--- docs/data-in-practice/plots.R
+++ docs/data-in-practice/plots.R
@@ -1,25 +1,38 @@
 library(dplyr)
+library(wesanderson)
 
-#df = read.csv("origin_answers.csv", sep=";")
+pallet = wes_palette("Royal2", 5, type = c("continuous"))
 
 create_pie_chart = function(df){
   
   res = df %>% group_by(origin) %>% summarise(Freq=n())
   
   slices = list(res$Freq)[[1]]
+  percentages = c(11.1, 11.1, 55.5, 11.11, 11.1)
   labels = list(res$origin)[[1]]
-  
-  chart = pie(slices, labels=labels, main="Origin of students")
-  
 
-  return (chart)
+  pie(slices
+  , labels=paste0(percentages, "%")
+  , col = pallet
+  , border = pallet
+  , main="Origin of students")
+  
+  
+  legend("topleft"
+  , legend = labels
+  , fill =  pallet
+  , border = pallet
+  , bg= "white")
+
+  return ()
 }
+
 
 create_bar_chart = function(df){
   
   counts = table(df$background)
   
-  mycols = c("red","blue")
+  mycols = wes_palette("Royal2", 2, type = c("discret"))
   
   par(mar=c(5,15,4,1)+.1)
   barplot(counts
@@ -28,6 +41,5 @@ create_bar_chart = function(df){
           , names.arg=c(df$background)
           , las = 1
           , col = mycols[df$group]
-          , legend = c("Technical", "Humanities")
-        )
+          , legend = c("Technical", "Humanities"))
 }
